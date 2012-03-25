f26ba6de9079a5fd2e2690952aafb824036c43b0
Assaf Israel <assafi@cs.technion.ac.il>
2012-03-25 14:29:57 +0200

    HostConversionTest.java

--- test/il/ac/technion/conversion/HostConversionTest.java
+++ test/il/ac/technion/conversion/HostConversionTest.java
@@ -7,8 +7,16 @@
 package il.ac.technion.conversion;
 
 import il.ac.technion.misc.Host;
+import il.ac.technion.misc.VM;
+import il.ac.technion.sla.SLABuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import junit.framework.Assert;
 
+import org.joda.time.Period;
 import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
@@ -16,25 +24,20 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 
 
 public class HostConversionTest {
+	
+	private final static String xmlFile = "test-resources//host.xml";
 
 	@Test
-	public void testConversion() {
-		Host h = new Host(1,10,100);
-		
-		String xmlDesc =
-			"<host>\n" + 
-			"  <bin>\n" + 
-			"    <id>1</id>\n" + 
-			"    <capacity>10</capacity>\n" + 
-			"  </bin>\n" +
-			"  <activationCost>100</activationCost>\n" +
-			"</host>";
+	public void testConversion() throws IOException {
+		Host h = new Host(1,10,100,Period.millis(1000));
+		h.assign(new VM(1,1,new SLABuilder(Period.millis(1000)).appEngineSLA()));		
 		
 		XStream xStream = new XStream(new DomDriver());
 		xStream.autodetectAnnotations(true);
-		Assert.assertEquals(xmlDesc,xStream.toXML(h));
 		
-		Host h2 = (Host)xStream.fromXML(xmlDesc);
+		xStream.toXML(h, new FileWriter(xmlFile));
+		
+		Host h2 = (Host)xStream.fromXML(new FileReader(xmlFile));
 		Assert.assertEquals(h,h2);
 	}
 }
--- test/il/ac/technion/sla/TableSLATest.java
+++ test/il/ac/technion/sla/TableSLATest.java
@@ -31,50 +31,51 @@ public class TableSLATest {
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidTable1() {
 		Map<Range,Double> badTable = new HashMap<Range, Double>();
-		badTable.put(new Range(100,0), -1.0);
+		badTable.put(new Range(0,100), -1.0);
 		new TableSLA(p,badTable);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidTable2() {
 		Map<Range,Double> badTable = new HashMap<Range, Double>();
-		badTable.put(new Range(99.0,0), 1.0);
+		badTable.put(new Range(0,99.0), 1.0);
 		new TableSLA(p,badTable);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidTable3() {
 		Map<Range,Double> badTable = new HashMap<Range, Double>();
-		badTable.put(new Range(101.0,0.0), 1.0);
+		badTable.put(new Range(0.0,101.0), 1.0);
 		new TableSLA(p,badTable);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidTable4() {
 		Map<Range,Double> badTable = new HashMap<Range, Double>();
-		badTable.put(new Range(100.0,99.99), 0.0);
-		badTable.put(new Range(99.95,99.0), 0.25);
-		badTable.put(new Range(99.0,95.0), 0.1);
-		badTable.put(new Range(95.0,0.0), 0.5);
+		badTable.put(new Range(99.99,100.0), 0.0);
+		badTable.put(new Range(99.0,99.95), 0.25);
+		badTable.put(new Range(95.0,99.0), 0.1);
+		badTable.put(new Range(0.0,95.0), 0.5);
 		new TableSLA(p,badTable);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testOverlap() {
 		Map<Range,Double> badTable = new HashMap<Range, Double>();
-		badTable.put(new Range(100.0,99.99), 0.0);
-		badTable.put(new Range(99.95,99.0), 0.1);
-		badTable.put(new Range(99.0,95.0), 0.25);
-		badTable.put(new Range(97.0,0.0), 0.5);
+		badTable.put(new Range(99.99,100.0), 0.0);
+		badTable.put(new Range(99.0,99.95), 0.1);
+		badTable.put(new Range(95.0,99.0), 0.25);
+		badTable.put(new Range(0.0,97.0), 0.5);
 		new TableSLA(p,badTable);
 	}
 	
+	@Test
 	public void testGoogleAppEngineSLA() {
 		Map<Range,Double> badTable = new HashMap<Range, Double>();
-		badTable.put(new Range(100.0,99.99), 0.0);
-		badTable.put(new Range(99.95,99.0), 0.1);
-		badTable.put(new Range(99.0,95.0), 0.25);
-		badTable.put(new Range(95.0,0.0), 0.5);
+		badTable.put(new Range(99.95,100.0), 0.0);
+		badTable.put(new Range(99.0,99.95), 0.1);
+		badTable.put(new Range(95.0,99.0), 0.25);
+		badTable.put(new Range(0.0,95.0), 0.5);
 		new TableSLA(p,badTable);
 	}
 }
