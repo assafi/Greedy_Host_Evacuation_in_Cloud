4d84a2d9d1230b366d734a5ffa2efb58e6f9fffb
Assaf Israel <assafi@cs.technion.ac.il>
2012-04-27 01:47:45 +0300

    Can now configure tests from XML

---
+++ test/il/ac/technion/config/TestTestConfiguration.java
@@ -0,0 +1,18 @@
+package il.ac.technion.config;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class TestTestConfiguration {
+
+	@Test
+	public void testGoodPath() throws XPathExpressionException, IOException, ParserConfigurationException, SAXException, ConfigurationException {
+		String filePath = getClass().getResource("test_config.xml").getPath();
+		new TestConfiguration(filePath);
+	}
+}
